9a7bfa8d8a8ad3618edae1b5de19b091786f55cb
Osbottel <41968614+Osbottel@users.noreply.github.com>
2023-11-06 15:26:38 +0000

    Initial commit

---
+++ word-creation/src/App.jsx
@@ -0,0 +1,31 @@
+import raw from "../../words_alpha.txt"
+
+export default function App() {
+
+  function extractData() {
+    fetch(raw)
+      .then(r => r.text())
+      .then(text => {
+      const textArray = text.split("\r\n").filter(reduceWords);
+      const sortedArray = textArray.sort(sortWords);
+      const objectArray = sortedArray.map(word => ({"word": word, "found": false}))
+      console.log(objectArray);
+      })
+  }
+
+    function reduceWords(word) {
+      return (word.length >= 3 && word.length <= 6)
+    }
+
+    function sortWords(word1, word2) {
+      return word2.length - word1.length || word1.localeCompare(word2);
+    }
+
+  extractData();
+
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
