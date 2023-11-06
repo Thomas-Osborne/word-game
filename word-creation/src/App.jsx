962387b8bf03c68d280e3ba8d9f738107e4fdca3
Osbottel <41968614+Osbottel@users.noreply.github.com>
2023-11-06 16:15:30 +0000

    Function to shuffle given word added

--- word-creation/src/App.jsx
+++ word-creation/src/App.jsx
@@ -1,4 +1,5 @@
 import raw from "../../words_alpha.txt"
+import {useState} from "react"
 
 export default function App() {
 
@@ -8,21 +9,17 @@ export default function App() {
       .then(text => {
       const textArray = text.split("\r\n").filter(reduceWords);
       const sortedArray = textArray.sort(sortWords);
-      const sixLetterArray = textArray.filter(getSixLetterWords)
-      console.log(sixLetterArray)
-      const index = randomIndex(sixLetterArray.length);
-      console.log(index);
-      const objectArray = sortedArray.map(word => ({"word": word, "found": false}))
+      const objectArray = sortedArray.map(word => ({"word": word.toUpperCase(), "found": false}))
       console.log(objectArray);
-      })
+    })
   }
 
     function reduceWords(word) {
       return (word.length >= 3 && word.length <= 6)
     }
 
-    function getSixLetterWords(word) {
-      return (word.length == 6)
+    function getNLetterWords(word, length) {
+      return (word.length == length)
     }
 
     function randomIndex(length) {
@@ -34,11 +31,23 @@ export default function App() {
       return word2.length - word1.length || word1.localeCompare(word2);
     }
 
-  extractData();
+    function shuffleWord(word) {
+      word = word.split("");
+      for (let i = word.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [word[i], word[j]] = [word[j], word[i]];
+      }
+      return word.join();
+    }
 
+  extractData();
+  // const wordList = extractData();
+  // console.log(wordList);
+  // console.log("Object", objectArray);
+  console.log(shuffleWord("abcdef"))
   return (
     <div>
-      Hello
+      <p>Hello</p>
     </div>
   )
 }
