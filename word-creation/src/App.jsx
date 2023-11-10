fee77483579b35525c19c6db43dc12ad59e9f562
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2023-11-10 12:43:14 +0000

    Added powerset function

--- word-creation/src/App.jsx
+++ word-creation/src/App.jsx
@@ -24,27 +24,48 @@ export default function App() {
     return (word.length >= 3 && word.length <= 6)
   }
 
-    function getNLetterWords(word, length) {
-      return (word.length == length)
-    }
+  function getNLetterWords(word, length) {
+    return (word.length == length)
+  }
 
-    function randomIndex(length) {
-      const index = Math.floor(Math.random() * (length - 1));
-      return index;
-    }
+  function randomIndex(length) {
+    const index = Math.floor(Math.random() * (length - 1));
+    return index;
+  }
 
-    function sortWords(word1, word2) {
-      return word2.length - word1.length || word1.localeCompare(word2);
-    }
+  function sortWords(word1, word2) {
+    return word2.length - word1.length || word1.localeCompare(word2);
+  }
 
-    function shuffleWord(word) {
-      word = word.split("");
-      for (let i = word.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [word[i], word[j]] = [word[j], word[i]];
-      }
-      return word.join();
+  function shuffleWord(word) {
+    word = word.split("");
+    for (let i = word.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [word[i], word[j]] = [word[j], word[i]];
     }
+    return word.join();
+  }
+
+  function getPowerSet(word) {
+    // Source: https://stackoverflow.com/questions/42773836/how-to-find-all-subsets-of-a-set-in-javascript-powerset-of-array
+    word = word.split("");
+    const subsets = [[]];
+    for (const char of word) {
+        const last = subsets.length-1;
+        for (let i = 0; i <= last; i++) {
+            let newSubset = [...subsets[i], char];
+            if (!(subsets.includes(newSubset))) {
+              subsets.push(newSubset);
+            }
+        }
+    }
+    
+    return subsets;
+  }
+
+  function getPermutations(word) {
+
+  }
 
   // const wordList = extractData();
   // console.log(wordList);
