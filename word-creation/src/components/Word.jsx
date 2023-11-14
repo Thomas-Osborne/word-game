5c39aa52160b100f40e1e7eba81633d1f091e452
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2023-11-14 17:53:39 +0000

    Letters are unrevealed when word found is false.

--- word-creation/src/components/Word.jsx
+++ word-creation/src/components/Word.jsx
@@ -3,7 +3,7 @@ import Letter from "./Letter"
 export default function Word(props) {
     
     const letters = props.word.name.split("");
-    const letterElts = letters.map((letter, index) => <Letter key={index} letter={letter}/>);
+    const letterElts = letters.map((letter, index) => <Letter key={index} letter={letter} found={props.word.found}/>);
 
     return (
         <div className="word-container">
