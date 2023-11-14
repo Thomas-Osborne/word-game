dfefc08bf96064629d90f670e6159ec333f38389
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2023-11-14 19:53:15 +0000

    Reset score on new word

--- word-creation/src/App.jsx
+++ word-creation/src/App.jsx
@@ -19,6 +19,7 @@ export default function App() {
 
   useEffect(() => {
     setFilteredWords(findAllWords(chosenWord));
+    setScore({points: 0, totalFound: 0});
   }, [chosenWord])
 
   function getRandomWord() {
