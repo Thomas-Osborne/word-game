dd491bc72c84cbd11a536311146e1445dac90831
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2023-11-16 20:59:19 +0000

    Can resign after restart

--- word-creation/src/components/GiveUp.jsx
+++ word-creation/src/components/GiveUp.jsx
@@ -5,13 +5,12 @@ import Icon from "./Icon"
 
 export default function GiveUp(props) {
     const [active, setActive] = useState(false);
-    const [isDisabled, setIsDisabled] = useState(false);
 
     const content = (
         <div>
             <h3>Do you really want to give up?</h3>
             <div>
-                <button className="button-menu button-primary" onClick={() => {toggle(); props.reveal(); setIsDisabled(true)}}>Yes</button>
+                <button className="button-menu button-primary" disabled={props.isRevealed} onClick={() => {toggle(); props.reveal()}}>Yes</button>
                 <button className="button-menu button-secondary" onClick={toggle}>No</button>
             </div>
         </div>
@@ -25,7 +24,7 @@ export default function GiveUp(props) {
         <Icon 
             toggle={toggle}
             active={active}
-            isDisabled={isDisabled}
+            isDisabled={props.isRevealed}
             buttonContent={<FontAwesomeIcon icon={faFlag} size="2x" />}
             buttonText="resign"
             heading="give up"    
