06d2fd28e725f0c482700c8cde27f1e77d0edce3
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2023-11-16 11:10:46 +0000

    Removed button borders of icons

--- word-creation/src/components/Header.jsx
+++ word-creation/src/components/Header.jsx
@@ -1,8 +1,19 @@
-export default function Header(props) {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
+export default function Header(props) {
     return (
         <header className="header">
             <h2 className="header-title">{props.title}</h2>
+            <div className = "main--buttons">
+                    <a href="https://github.com/Thomas-Osborne" target="_blank">
+                        <button className="button-icon"><FontAwesomeIcon icon={faGithub} size="2x" /></button>
+                    </a>
+                    <a href="https://www.linkedin.com/in/tom-osborne-716619288/" target="_blank">
+                        <button className="button-icon"><FontAwesomeIcon icon={faLinkedin} size="2x" /></button>
+                    </a>
+            </div>
         </header>
     )
 }
