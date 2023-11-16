a7710b76e4a170792ce5875a6f944b858907e91e
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2023-11-16 12:56:03 +0000

    Added modal template

--- word-creation/src/components/Instructions.jsx
+++ word-creation/src/components/Instructions.jsx
@@ -1,13 +1,26 @@
+import { useState } from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBookOpen } from '@fortawesome/free-solid-svg-icons'
+import Modal from "./Modal"
 
 export default function Instructions() {
+    const [active, setActive] = useState(true);
+
+    function toggle() {
+        setActive(prevActive => !prevActive);
+    }
+
     return (
         <div className="instructions-container">
             <button className="button-icon button-instructions">
-            <FontAwesomeIcon icon={faBookOpen} size="2x" />
+                <FontAwesomeIcon icon={faBookOpen} size="2x" />
             </button>
             <p className="instructions-text">HOW</p>
+            <Modal 
+                modal={active}
+                toggle={toggle}
+                heading="How to Play"
+            />
         </div>
     )
 }
---
+++ word-creation/src/components/Modal.jsx
@@ -0,0 +1,24 @@
+import {useState} from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons'
+
+export default function Modal(props) {
+
+    return (
+        <div>
+            {props.modal && (
+            <div className="modal">
+                <div className="overlay">
+                    <div className="modal-content">
+                        <h1 className="modal-heading">{props.heading}</h1>
+                        <p>This is my modal. Hello everybody.</p>
+                        <button className="button-icon close-modal" onClick={props.toggle}>
+                            <FontAwesomeIcon icon={faCircleXmark} size="2x" />
+                        </button>
+                    </div>
+                </div>
+            </div>
+            )}
+        </div>
+    )
+}
