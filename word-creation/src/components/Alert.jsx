803b343782e73280099db7829f0beedb9b514640
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2023-11-16 14:46:07 +0000

    Added gradient colours to alert box

--- word-creation/src/components/Alert.jsx
+++ word-creation/src/components/Alert.jsx
@@ -1,6 +1,10 @@
 export default function Alert(props) {
     return (
-        <div className="alert-container" style={{borderColor: props.alert.error ? 'red' : 'green'}}>
+        <div 
+            className="alert-container" 
+            style={{background: props.alert.error 
+                ? 'linear-gradient(#D65144, #CB6A61)' 
+                : 'linear-gradient(#417944, #7EB581)'}}>
             <p className="alert-text">{props.alert.message}</p>
         </div>
     )
