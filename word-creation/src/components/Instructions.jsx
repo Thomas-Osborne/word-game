7787eeb04a47f5e7769a889d6f2581b2c87f5061
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2023-11-17 15:15:40 +0000

    Updated word pane to make easier to maintain

--- word-creation/src/components/Instructions.jsx
+++ word-creation/src/components/Instructions.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBookOpen } from '@fortawesome/free-solid-svg-icons'
 import Icon from "./Icon"
+import Word from "./Word"
 
 export default function Instructions(props) {
     const [active, setActive] = useState(true);
@@ -9,10 +10,15 @@ export default function Instructions(props) {
 
     const content = (
         <div>
-            <h3>Welcome to the Word Game!</h3>
-            <p>There are 6 letters for you to play with, you need to make as many words out of them as possible. You can give up and reveal the words, but then you will not be able to play!</p>
-            <h4>Scoring</h4>
-            <p>If you get a {props.max} letter word you score {2 * props.max} points. Otherwise you score as many points as the number of letters.</p>
+            <ul className="unbulleted-list">
+                <Word word={{name: "RULES", found: true, revealed: true}} />
+            </ul>
+            <p className="rules-text">There are 6 letters for you to play with, you need to make as many words out of them as possible. You can give up and reveal the words, but then you will not be able to play!</p>
+
+            <ul className="unbulleted-list">
+                <Word word={{name: "SCORE", found: true, revealed: true}} />
+            </ul>
+            <p className="rules-text">If you get a {props.max} letter word you score {2 * props.max} points. Otherwise you score as many points as the number of letters.</p>
             <button className="button-menu button-primary" onClick={toggle}>Okay!</button>
         </div>
     )
